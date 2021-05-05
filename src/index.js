da9e77bb807f269eb9d4757ce5c29a713c355c95
elena-ni <niederfuehr@gmail.com>
2021-05-05 19:23:10 +0000

    fix icons

--- src/index.js
+++ src/index.js
@@ -48,8 +48,18 @@ function displayWeatherCondition(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].description;
+  response.data.weather[0].description;
+
+  let iconElement = document.querySelector("#icon");
+  
+    iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
 }
 
 // current location
